3cd7c0c39aee474da7dca9c6965f10b98332f313
CadoganPL <cadogansea@gmail.com>
2017-09-21 13:53:50 +0200

    #januszCommit

---
+++ Car Simulator/Assets/CameraPivot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivot : MonoBehaviour {
+    [SerializeField]
+    private GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position + target.GetComponent<Rigidbody>().velocity; 
+
+	}
+}
